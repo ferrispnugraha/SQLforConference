86d17fc494466dd86b1dde12c5095c1af0d1a4dc
schwarz11 <49577069+schwarz11@users.noreply.github.com>
2020-08-27 20:42:54 +0700

    Add files via upload

---
+++ task2create.sql
@@ -0,0 +1,164 @@
+/*  Name: Ferris Prima NUGRAHA
+    SID : 20543826
+    COMP 3311 Summer 2020
+    Task 2 Create Relation Schemas
+*/
+
+clear screen
+set feedback off
+set heading off
+select '*** Creating Task 2 database ***' from dual;
+set heading on
+
+-- Clean the database
+
+drop table Discussion;
+drop table Review;
+drop table Assigned;
+drop table Prefers;
+drop table AuthorOf;
+drop table Submission;
+drop table PCChair;
+drop table PCMember;
+drop table Person;
+
+-- Create the tables
+create table Person(
+    personId    smallint        primary key,
+    username    char(10)        default null,
+    title       char(5),
+    name        varchar2(50),
+    institution varchar2(100),
+    country     varchar2(30),
+    email       varchar2(50),
+    
+    -- constraint title to be Mr, Ms, Miss, Dr, Prof; default is null
+    constraint PersonTitle check(title in ('Mr', 'Ms', 'Miss', 'Dr', 'Prof')),
+    -- constraint unique email
+    constraint UniqueEmail unique(email),
+    -- constraint username's length is minimum 3 and maximum 10 lowercase letters; default is null
+    constraint OnlyLowercaseUsername check(regexp_like(username, '^[a-z]{3,10}\s*$')),
+    -- constraint unique username
+    constraint UniqueUsername unique(username));
+
+create table PCMember(
+    personId    smallint    primary key,
+    
+    -- inherited from Person
+    constraint PCMemberForeign foreign key (personId) references Person(personId) on delete cascade);
+    
+create table PCChair(
+    personId    smallint    primary key,
+    
+    --inherited from PCMember
+    constraint PCChairForeign foreign key (personId) references PCMember(personId) on delete cascade);
+    
+create table Submission(
+    submissionNo    smallint        primary key,
+    title           varchar2(100)   default null,
+    abstract        varchar2(300),
+    submissionType  varchar2(20)    default 'research',
+    status          varchar2(9)     default null,
+    contactAuthor   smallint,
+    
+    -- constraint submissionType to be demo, industrial, research, vision; default is research
+    constraint SubmissionType check(submissionType in ('demo', 'industrial', 'research', 'vision')),
+    -- constraint status to be accept, reject, withdrawn; default is null
+    constraint SubmissionStatus check(status in ('accept', 'reject', 'withdrawn')),
+    
+    -- for the "ContactFor" relation
+    constraint SubmissionForeign foreign key (contactAuthor) references Person(personId) on delete cascade);
+
+create table AuthorOf(
+    submissionNo    smallint,
+    personId        smallint,
+    primary key (submissionNo, personId),
+    
+    -- between Submission and Person
+    constraint AuthorOfForeign1 foreign key (submissionNo) references Submission(submissionNo) on delete cascade,
+    constraint AuthorOfForeign2 foreign key (personId) references Person(personId) on delete cascade);
+    
+create table Prefers(
+    submissionNo    smallint,
+    personId        smallint,
+    preference      smallint    default null,
+    primary key(submissionNo, personId),
+    
+    -- constraint preference to be 1,2,3,4,5; default is null
+    constraint Prefer1To5 check(preference in (1,2,3,4,5)),
+    
+    -- between Submission and PCMember
+    constraint PrefersForeign1 foreign key (submissionNo) references Submission(submissionNo) on delete cascade,
+    constraint PrefersForeign2 foreign key (personId) references PCMember(personId) on delete cascade);
+
+create table Assigned(
+    submissionNo    smallint,
+    personId        smallint,
+    primary key (submissionNo, personId),
+    
+    -- between Submission and PCMember
+    constraint AssignedForeign1 foreign key (submissionNo) references Submission(submissionNo) on delete cascade,
+    constraint AssignedForeign2 foreign key (personId) references PCMember(personId) on delete cascade);
+
+create table Review(
+    submissionNo            smallint,
+    personId                smallint,
+    
+    relevant                char(1) not null,
+    technicallyCorrect      char(1) not null,
+    lengthAndContent        char(1) not null,
+    
+    confidence              number(2,1) not null,
+    
+    originality             smallint not null,
+    impact                  smallint not null,
+    presentation            smallint not null,
+    technicalDepth          smallint not null,
+    overallRating           smallint not null,
+    
+    mainContribution        varchar2(300),
+    strongPoints            varchar2(300),
+    weakPoints              varchar2(300),
+    overallSummary          varchar2(300),
+    detailedComments        varchar2(1000),
+    confidentialComments    varchar2(300),
+    primary key (submissionNo, personId),
+    
+    -- constraints as mentioned in Appendix A
+    -- relevant, technicallyCorrect, lengthAndContent to be Y/N/M, default null
+    constraint RelevantYNM check(relevant in('Y','N','M')),
+    constraint TechnicallyCorrectYNM check(technicallyCorrect in('Y','N','M')),
+    constraint LengthAndContentYNM check(lengthAndContent in('Y','N','M')),
+    
+    -- confidence between 0.5 and 1
+    constraint ConfidenceHalfTo5 check(confidence between 0.5 and 1),
+    
+    -- originality, impact, presentation, technicalDepth, overallRating between 1 and 5
+    constraint Originality1To5 check(originality in (1,2,3,4,5)),
+    constraint Impact1To5 check(impact in (1,2,3,4,5)),
+    constraint Presentation1To5 check(presentation in (1,2,3,4,5)),
+    constraint TechnicalDepth1To5 check(technicalDepth in (1,2,3,4,5)),
+    constraint Rating1To5 check(overallRating in (1,2,3,4,5)),
+    
+    -- between Submission and PCMember
+    constraint ReviewForeign1 foreign key (submissionNo) references Submission(submissionNo) on delete cascade,
+    constraint ReviewForeign2 foreign key (personId) references PCMember(personId) on delete cascade);
+
+create table Discussion(
+    sequenceNo      smallint,
+    submissionNo    smallint,
+    personId        smallint,
+    comments        varchar2(200),
+    primary key (sequenceNo, submissionNo, personId),
+    
+    -- between Submission and PCMember
+    constraint DisucssionForeign1 foreign key (submissionNo) references Submission(submissionNo) on delete cascade,
+    constraint DiscussionForeign2 foreign key (personId) references PCMember(personId) on delete cascade);
+
+/* Write data to the disk */
+commit;
+
+set feedback off
+set heading off
+select '*** Task 2 database created  ***' from dual;
+set heading on
---
+++ task2query.sql
@@ -0,0 +1,118 @@
+/*  Name: Ferris Prima NUGRAHA
+    SID : 20543826
+    COMP 3311 Summer 2020
+    Task 2 Query 
+*/
+
+
+/*
+    1. For each submission that does not have any PC member assigned to it and that is available for assignment,
+       find the title, submission type, and contact author name and email. Order the result by submission type
+       ascending.
+*/
+
+set heading off
+select 'Query 1: For each submission that does not have any PC member assigned to it and that is available for assignment,
+         find the title, submission type, and contact author name and email. Order the result by submission type
+         ascending.' from dual;
+set heading on
+
+with temp(submissionNo, submissionTitle, submissionType, personId, contactAuthor) as -- personId is the assigned PCMember
+(   select submissionNo, title, submissionType, personId, contactAuthor
+    from Submission natural left outer join Assigned
+    where status <> 'withdrawn')                                    -- not withdrawn submission
+
+select submissionTitle, submissionType, name, email
+from temp join Person on (temp.contactAuthor = Person.personId)     -- to link contactAuthor to find his/her email and name
+group by submissionNo, submissionTitle, submissionType, name, email
+having count(temp.personId) = 0                                    -- no PCMember is assigned
+order by submissionNo asc;
+
+/*
+    2. Find the submission number, title, and names of all the authors, for those submissions that have at least one
+       PC member as an author. Order the result by submission number ascending.
+*/
+
+set heading off
+select '------------------------------------------------------------------------------------------------------' from dual;
+select 'Query 2: Find the submission number, title, and names of all the authors, for those submissions that have at least one
+         PC member as an author. Order the result by submission number ascending.' from dual;
+set heading on
+    
+select submissionNo, title, name
+from  AuthorOf natural join Person
+where submissionNo in (select submissionNo
+                        from Submission natural join AuthorOf natural join PCMember)  --if exist after natural join, at least an author is PCMember
+order by submissionNo asc;
+
+
+/*
+    3. For each PC member, find the PC member name and the number of submissions for which he/she has
+       specified a preference. Order the result by number of submissions descending.
+*/
+
+set heading off
+select '------------------------------------------------------------------------------------------------------' from dual;
+select 'Query 3: For each PC member, find the PC member name and the number of submissions for which he/she has
+         specified a preference. Order the result by number of submissions descending.' from dual;
+set heading on
+
+with result(personId, SubmissionCount) as 
+(   select personId, count(submissionNo)
+    from PCMember natural join Prefers
+    where preference between 1 and 5
+    group by personId)
+
+select name, SubmissionCount
+from result natural join Person
+order by SubmissionCount desc;
+
+/*
+    4. For those PC members who have not indicated a preference of 3 or greater for any submission, find the PC
+       member name as well as the submission number, title and preference of the submissions for which they
+       have indicated a preference. Order the result first by PC member name in ascending order and then by
+       preference for a submission in descending order.
+*/
+
+set heading off
+select '------------------------------------------------------------------------------------------------------' from dual;
+select 'Query 4: For those PC members who have not indicated a preference of 3 or greater for any submission, find the PC
+         member name as well as the submission number, title and preference of the submissions for which they
+         have indicated a preference. Order the result first by PC member name in ascending order and then by
+         preference for a submission in descending order.' from dual;
+set heading on
+
+with recap(personId, submissionNo, SubmissionTitle, preference) as
+(   select personId, submissionNo, title, preference
+    from PCMember natural join Prefers natural join Submission)
+
+select name, submissionNo, SubmissionTitle, preference
+from recap natural join Person
+where personId not in (select personId
+                       from recap
+                       where preference >= 3)
+order by name asc, preference desc;
+
+/*
+    5. For each submission that has at least three reviews, find the title, submission type, PC member name, overall
+       rating and spread where the spread is 1.0 or greater. Order the result by submission number.
+*/
+
+set heading off
+select '------------------------------------------------------------------------------------------------------' from dual;
+select 'Query 5: For each submission that has at least three reviews, find the title, submission type, PC member name, overall
+         rating and spread where the spread is 1.0 or greater. Order the result by submission number.' from dual;
+set heading on
+
+with spreadRecap(submissionNo, submissionTitle, submissionType, ReviewNumber, Spread) as 
+(   select submissionNo, title, submissionType, count(*), max(overallRating)- min(overallRating)
+    from Submission natural join Review
+    group by submissionNo, title, submissionType)
+    
+select submissionTitle, submissionType, name, overallRating, Spread
+from spreadRecap natural join Review natural join Person
+where submissionNo in (select submissionNo
+                        from spreadRecap
+                        where ReviewNumber >= 3
+                        and Spread >= 1.0)
+order by submissionNo;
